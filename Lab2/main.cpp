808a7e30810d766b53f533632ea93dbe44d28aef
Krzysztof Ruczkowski <tojatos@gmail.com>
2019-10-24 14:09:14 +0200

    Lab2 couts added

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -15,9 +15,13 @@ int main()
 {
     Table t;
     t.setName("Arr");
+    std::cout << "Mod tab start:\n";
     mod_tab(t, 6);
+    std::cout << "Mod tab end.\n";
     Table *t2 = t.clone();
+    std::cout << "Mod tab pointer start:\n";
     mod_tab(t2, 7);
+    std::cout << "Mod tab pointer end.\n";
     delete t2;
 
     Table t3("Table", 1);
