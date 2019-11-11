5ea1ce69c69082319a420e780eab1c829060b455
tojatos <tojatos@gmail.com>
2019-11-11 23:18:34 +0100

    Cmake user file ignored

--- copy_project.sh
+++ copy_project.sh
@@ -5,7 +5,6 @@ if [[ -z $1 ]] || [[ -z $2 ]]; then
   echo "Usage: $(basename $0) OLD_PROJECT_DIR NEW_PROJECT_DIR"
 fi
 
-rsync -av --exclude build $1/ $2
+rsync -av --exclude build --exclude CMakeLists.txt.user $1/ $2
 cd $2
 sed "s/$1/$2/g" CMakeLists.txt -i
-sed "s/$1/$2/g" CMakeLists.txt.user -i
