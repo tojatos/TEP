f1f7cd1d893cf4d34312f6ddaa0d8f1200ffb13a
tojatos <tojatos@gmail.com>
2019-12-07 00:19:18 +0100

    Lab9 - basic test function added

--- Lab9/main.cpp
+++ Lab9/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include "constants.h"
+#include "mscnproblem.h"
+
+void test()
+{
+    MscnProblem problem;
+    problem.setDCount(2);
+    problem.setFCount(1);
+    problem.setMCount(3);
+    problem.setSCount(4);
+
+
+}
 
 int main()
 {
+    test();
     return 0;
 }
