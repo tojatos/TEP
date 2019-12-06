437a59feca583316ed43818813a67724d0f3fe3d
tojatos <tojatos@gmail.com>
2019-12-07 00:48:30 +0100

    Lab9 - test function improved (saving tested)

--- Lab9/main.cpp
+++ Lab9/main.cpp
@@ -5,12 +5,28 @@
 void test()
 {
     MscnProblem problem;
-    problem.setDCount(2);
-    problem.setFCount(1);
-    problem.setMCount(3);
-    problem.setSCount(4);
+    const int testDCount = 2;
+    const int testFCount = 1;
+    const int testMCount = 3;
+    const int testSCount = 4;
+    problem.setDCount(testDCount);
+    problem.setFCount(testFCount);
+    problem.setMCount(testMCount);
+    problem.setSCount(testSCount);
 
+    for(int i = 0; i < testDCount; ++i)
+        for(int j = 0; j < testFCount; ++j)
+            problem.setInCd(i*testDCount+j, i, j);
 
+    for(int i = 0; i < testFCount; ++i)
+        for(int j = 0; j < testMCount; ++j)
+            problem.setInEf(i*testFCount+j, i, j);
+
+    for(int i = 0; i < testMCount; ++i)
+        for(int j = 0; j < testSCount; ++j)
+            problem.setInCm(i*testMCount+j, i, j);
+
+    problem.save("test.txt");
 }
 
 int main()
